d78355fc72046c962145d8425f7084e07e6065c2
BenDev181 <benjiro181@gmail.com>
2024-10-14 13:20:02 +0100

    Day 1 tasks completed

--- index.test.js
+++ index.test.js
@@ -11,12 +11,19 @@ const {seedMusician} = require("./seedData");
 
 
 describe('./musicians endpoint', () => {
-    // Write your tests here
-    
-    
+    test("Testing musicians endpoint status code", async () => {
+        // Sends request to `/musicians` endpoint
+        const response = await request(app).get("/musicians");
+        expect(response.statusCode).toBe(200);
+    })
 
-
-
-
-    
+    test("Testing musicians endpoint", async () => {
+        // Sends request to `/musicians` endpoint
+        const response = await request(app).get("/musicians");
+        const responseData = JSON.parse(response.text);
+        console.log(responseData)
+        expect(responseData.length).toBe(3)
+        expect(responseData[0].id).toBe(1)
+        expect(responseData[0].name).toBe("Mick Jagger")
+    })
 })
