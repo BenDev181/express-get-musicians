bb8ff3d61c8fa652da4375e3e345ee447030232d
BenDev181 <benjiro181@gmail.com>
2024-10-15 12:11:53 +0100

    Day 2 tasks complete

--- src/app.js
+++ src/app.js
@@ -5,11 +5,34 @@ const { db } = require("../db/connection")
 
 const port = 3000;
 
-//TODO: Create a GET /musicians route to return all musicians 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.get("/musicians", async (request, response) => {
     let musicians = await Musician.findAll()
     response.json(musicians)
 })
+
+app.get("/musicians/:id", async (req, res) => {
+    let musician = await Musician.findByPk(req.params.id)
+    res.json(musician)
+})
+
+app.post("/musicians", async (req, res) => {
+    const musician = await Musician.create(req.body)
+    res.json(musician)
+})
+
+app.put("/musicians/:id", async (req, res) => {
+    const updatedMusician = await Musician.update(req.body, {where: {id: req.params.id}});
+    res.json(updatedMusician)
+})
+
+app.delete("/musicians/:id", async (req, res) => {
+    const deletedMusician = await Musician.destroy({where: {id: req.params.id}})
+    res.json(deletedMusician)
+})
+
 
 
 
